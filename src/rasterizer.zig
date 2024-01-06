7d9e698fa12a3fa700b6712230ab0dfb5e7c7e0c
Maxime Guillemette <maxime_glmt@hotmail.ca>
2024-01-05 19:38:03 -0500

    shaded triangles

--- src/rasterizer.zig
+++ src/rasterizer.zig
@@ -17,7 +17,8 @@ const Black = Color {.r = 0, .g = 0, .b = 0};
 const White = Color {.r = 0xff, .g = 0xff, .b = 0xff};
 const Green = Color {.r = 0, .g = 0xff, .b = 0};
 
-const interp_buf_size = nextCapacity(@as(usize, @max(width, height))) * @sizeOf(usize) * 2;
+// interpolating a single shaded triangle could potentially take 3*2*width*height elements
+const interp_buf_size = nextCapacity(@as(usize, @max(width, height))) * @sizeOf(usize) * 6;
 var interp_buffer_mem: [interp_buf_size]u8 = undefined;
 
 // Geometry stuff
@@ -53,6 +54,13 @@ const Triangle = struct {
     }
 };
 
+const ShadedTriangle = struct {
+    triangle: Triangle,
+    h0: float,
+    h1: float,
+    h2: float,
+};
+
 // global variables
 var canvas: utils.Canvas = .{
     .width = width,
@@ -83,7 +91,7 @@ fn drawLine(p0: Coord2D, p1: Coord2D, color: Color, allocator: Allocator) !void 
             dep1 = p0.y;
         }
 
-        try interpolate(ind0, dep0, ind1, dep1, &list);
+        try interpolate(usize, ind0, dep0, ind1, dep1, &list);
 
         const start: usize = @intFromFloat(ind0);
         const end: usize = @intFromFloat(ind1);
@@ -104,7 +112,7 @@ fn drawLine(p0: Coord2D, p1: Coord2D, color: Color, allocator: Allocator) !void 
             dep1 = p0.x;
         }
 
-        try interpolate(ind0, dep0, ind1, dep1, &list);
+        try interpolate(usize, ind0, dep0, ind1, dep1, &list);
 
         const start: usize = @intFromFloat(ind0);
         const end: usize = @intFromFloat(ind1);
@@ -114,7 +122,33 @@ fn drawLine(p0: Coord2D, p1: Coord2D, color: Color, allocator: Allocator) !void 
     }
 }
 
-fn interpolate(ind0: float, dep0: float, ind1: float, dep1: float, list: *std.ArrayList(usize)) !void {
+fn interpolate(comptime T: type, ind0: float, dep0: float, ind1: float, dep1: float, list: *std.ArrayList(T)) !void {
+    if (T == float) {
+        return interpolate_float(ind0, dep0, ind1, dep1, list);
+    } else {
+        return interpolate_usize(ind0, dep0, ind1, dep1, list);
+    }
+}
+
+fn interpolate_float(ind0: float, dep0: float, ind1: float, dep1: float, list: *std.ArrayList(float)) !void {
+    // corner case
+    if (ind0 == ind1) {
+        try list.append(dep0);
+        return;
+    }
+
+    const a = (dep1 - dep0) / (ind1 - ind0);
+    var d = dep0;
+
+    const start: usize = @intFromFloat(ind0);
+    const end: usize = @intFromFloat(ind1);
+    for (start..end) |_| {
+        try list.append(d);
+        d += a;
+    }
+}
+
+fn interpolate_usize(ind0: float, dep0: float, ind1: float, dep1: float, list: *std.ArrayList(usize)) !void {
     // corner case
     if (ind0 == ind1) {
         try list.append(@intFromFloat(dep0));
@@ -144,16 +178,17 @@ pub fn drawFillTriangle(triangle: *Triangle, color: Color, allocator: Allocator)
 
     // compute X coords of edges
     var x01 = std.ArrayList(usize).init(allocator);
+    defer x01.deinit();
     var x12 = std.ArrayList(usize).init(allocator);
+    defer x12.deinit();
     var x02 = std.ArrayList(usize).init(allocator);
+    defer x02.deinit();
 
-    try interpolate(triangle.p0.y, triangle.p0.x, triangle.p1.y, triangle.p1.x, &x01);
-    try interpolate(triangle.p1.y, triangle.p1.x, triangle.p2.y, triangle.p2.x, &x12);
-    try interpolate(triangle.p0.y, triangle.p0.x, triangle.p2.y, triangle.p2.x, &x02);
+    try interpolate(usize, triangle.p0.y, triangle.p0.x, triangle.p1.y, triangle.p1.x, &x01);
+    try interpolate(usize, triangle.p1.y, triangle.p1.x, triangle.p2.y, triangle.p2.x, &x12);
+    try interpolate(usize, triangle.p0.y, triangle.p0.x, triangle.p2.y, triangle.p2.x, &x02);
 
     // concatenate short sides
-    print("before concatenation x01 has len {} and x02 has len {}\n", .{x01.items.len, x12.items.len});
-    //_ = x01.pop();
     try x01.appendSlice(x12.items);
 
     // determine which is left and which is right
@@ -163,20 +198,85 @@ pub fn drawFillTriangle(triangle: *Triangle, color: Color, allocator: Allocator)
     if (x02.items[middle] < x01.items[middle]) {
         x_left = x02.items;
         x_right = x01.items;
-        print("x02 is left, x01 is right\n", .{});
     }
 
     // draw horizontal segments to fill the triangle
     const y_start: usize = @intFromFloat(triangle.p0.y);
     const y_end: usize = @intFromFloat(triangle.p2.y);
-    print("filling triangle from {} to {}\n", .{y_start, y_end});
-    print("lens are {} and {}\n", .{x_left.len, x_right.len});
     for (y_start..y_end) |y| {
-        print("triangle fill line {}\n", .{y - y_start});
         const x_start = x_left[y - y_start];
         const x_end = x_right[y - y_start];
         for (x_start..x_end) |x| {
             canvas.put(x, y, color);
+        }
+    }
+}
+
+pub fn drawShadedTriangle(shaded_triangle: *ShadedTriangle, color: Color, allocator: Allocator) !void {
+    var triangle = &shaded_triangle.triangle;
+
+    // sort vertices
+    triangle.sortY();
+
+    // compute X coords of edges
+    var x01 = std.ArrayList(usize).init(allocator);
+    defer x01.deinit();
+    var x12 = std.ArrayList(usize).init(allocator);
+    defer x12.deinit();
+    var x02 = std.ArrayList(usize).init(allocator);
+    defer x02.deinit();
+    var h01 = std.ArrayList(float).init(allocator);
+    defer h01.deinit();
+    var h12 = std.ArrayList(float).init(allocator);
+    defer h12.deinit();
+    var h02 = std.ArrayList(float).init(allocator);
+    defer h02.deinit();
+
+    try interpolate(usize, triangle.p0.y, triangle.p0.x, triangle.p1.y, triangle.p1.x, &x01);
+    try interpolate(usize, triangle.p1.y, triangle.p1.x, triangle.p2.y, triangle.p2.x, &x12);
+    try interpolate(usize, triangle.p0.y, triangle.p0.x, triangle.p2.y, triangle.p2.x, &x02);
+
+    try interpolate(float, triangle.p0.y, shaded_triangle.h0, triangle.p1.y, shaded_triangle.h1, &h01);
+    try interpolate(float, triangle.p1.y, shaded_triangle.h1, triangle.p2.y, shaded_triangle.h2, &h12);
+    try interpolate(float, triangle.p0.y, shaded_triangle.h0, triangle.p2.y, shaded_triangle.h2, &h02);
+
+    // concatenate short sides
+    try x01.appendSlice(x12.items);
+    try h01.appendSlice(h12.items);
+
+    // determine which is left and which is right
+    const middle = x01.items.len / 2;
+    var x_left = x01.items;
+    var x_right = x02.items;
+    var h_left = h01.items;
+    var h_right = h02.items;
+
+    if (x02.items[middle] < x01.items[middle]) {
+        x_left = x02.items;
+        x_right = x01.items;
+        h_left = h02.items;
+        h_right = h01.items;
+    }
+
+    // draw horizontal segments to fill the triangle
+    const y_start: usize = @intFromFloat(triangle.p0.y);
+    const y_end: usize = @intFromFloat(triangle.p2.y);
+    for (y_start..y_end) |y| {
+        const x_start = x_left[y - y_start];
+        const x_end = x_right[y - y_start];
+        var h_segment = std.ArrayList(float).init(allocator);
+        defer h_segment.deinit();
+
+        try interpolate(
+            float,
+            @floatFromInt(x_start),
+            h_left[y - y_start],
+            @floatFromInt(x_end),
+            h_right[y - y_start],
+            &h_segment
+        );
+        for (x_start..x_end) |x| {
+            canvas.put(x, y, color.apply_light(h_segment.items[x - x_start]));
         }
     }
 }
@@ -186,22 +286,29 @@ pub fn main() !void {
     print("canvas is {d} bytes large, w={} h={}\n", .{canvas.pixels.len, width, height});
 
     const p0: Coord2D = .{.x = 10, .y = 10};
-    const p1: Coord2D = .{.x = 200, .y = 50};
-    const p2: Coord2D = .{.x = 50, .y = 75};
+    const p1: Coord2D = .{.x = 350, .y = 50};
+    const p2: Coord2D = .{.x = 50, .y = 230};
     //const p3: Coord2D = .{.x = 350, .y = 15};
-    var t0: Triangle = .{.p0=p0, .p1=p1, .p2=p2};
+    var t0: ShadedTriangle = .{
+        .triangle = .{
+            .p0=p0, .p1=p1, .p2=p2,
+        },
+        .h0 = 1,
+        .h1 = 0.5,
+        .h2 = 0,
+    };
 
 
     // static buffer allocator for interpolation function
     var fba = std.heap.FixedBufferAllocator.init(&interp_buffer_mem);
 
     try drawWireTriangle(
-        t0,
+        t0.triangle,
         White,
         fba.allocator(),
     );
 
-    try drawFillTriangle(
+    try drawShadedTriangle(
         &t0,
         Green,
         fba.allocator(),
